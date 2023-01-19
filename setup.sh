ebdd873814d8a7bbfa3ba29cafc080d417a5dad5
Andrew Naylor <anaylor@lbl.gov>
2023-01-19 11:28:09 -0800

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -12,15 +12,14 @@
 ## Variables
 CONDA_RAY_ENV=ray_ml_env
 module load python
-CONDA_BIN=mamba
 
 ## Create
 echo '<> Building new env...'
-$CONDA_BIN create -n $CONDA_RAY_ENV ipykernel python=3.8.13 -y #Set the version to match latest nersc pytorch image
+mamba create -n $CONDA_RAY_ENV ipykernel python=3.8.13 -y #Set the version to match latest nersc pytorch image
 
 ## Activate
 echo '<> Activating conda env'
-$CONDA_BIN activate $CONDA_RAY_ENV
+conda activate $CONDA_RAY_ENV
 
 echo '<> Install libraries via pip'
 python3 -m pip install -r requirements.txt
@@ -29,4 +28,4 @@ python3 -m pip install -r requirements.txt
 echo '<> Installing kernel in JupyterHub'
 python3 -m ipykernel install --user --name $CONDA_RAY_ENV --display-name $CONDA_RAY_ENV
 
-echo "<!> Setup complete, to activate conda env: source activate $CONDA_RAY_ENV"
+echo "<!> Setup complete, to activate conda env: conda activate $CONDA_RAY_ENV"
