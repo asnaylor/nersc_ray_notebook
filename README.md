f0c394178b44b9f5f74d475e0d9b269272a95049
Andrew Naylor <anaylor@lbl.gov>
2023-03-03 18:33:56 -0800

    Switch to module load instead of shifter

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ These example [notebooks](notebooks) will cover how different machine learning f
 
 |     | Notebook | Description |
 | :-- | :----- | :---------- |
-| 1  | [PyTorch MNIST Example: Ray + Horovod](notebooks/ex_01_pytorch_ray_hvd.ipynb) | Deploying a Ray cluster via shifter in order to run Ray with Horovod. |
-| 2  | [Tuning Hyperparameters of a Distributed PyTorch Model with PBT using Ray Train & Tune](notebooks/ex_02_pytorch_ray_train_tune.ipynb) | Deploying a Ray cluster via shifter in order to do Distributed Tunning of Hyperparameters with PyTorch. |
-| 3  | [Tuning Hyperparameters of a Distributed TensorFlow Model using Ray Train & Tune](notebooks/ex_03_tensorflow_ray_train_tune.ipynb) | Deploying a Ray cluster via shifter in order to do Distributed Tunning of Hyperparameters with TensorFlow. |
+| 1  | [Tuning Hyperparameters of a Distributed PyTorch Model with PBT using Ray Train & Tune](notebooks/ex_01_pytorch_ray_train_tune.ipynb) | Deploying a Ray cluster in order to do Distributed Tunning of Hyperparameters with PyTorch. |
+| 2  | [Tuning Hyperparameters of a Distributed TensorFlow Model using Ray Train & Tune](notebooks/ex_02_tensorflow_ray_train_tune.ipynb) | Deploying a Ray cluster in order to do Distributed Tunning of Hyperparameters with TensorFlow. |
+| 3  | [PyTorch MNIST Example: Ray + Horovod](notebooks/ex_03_pytorch_ray_hvd.ipynb) | Deploying a Ray cluster in order to run Ray with Horovod. |
+
 
 > **Note**
-> To setup the environment for each notebook, execute on command line: `source setup.sh <exercise-number> [-f]` (e.g `source setup.sh 1`).
+> To setup the environment for each notebook, execute on command line: `./setup.sh <exercise-number>` (e.g `./setup.sh 1`).
--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@
 
 ## Parse Args
 usage() {                                 
-    echo "Usage: source setup.sh <Exercise Number> [-f]" 1>&2 
+    echo "Usage: ./setup.sh <Exercise Number> [--dev]" 1>&2 
 }
 
 exit_abnormal() { 
@@ -24,107 +24,65 @@ if [ $# -lt 1 ]; then
     exit_abnormal
 fi
 
-setup_conda() {
-    CONDA_RAY_ENV=nersc_cluster_deploy
-    module load python
-    #Create conda env
-    conda create -n $CONDA_RAY_ENV ipykernel python=3.10 -y
-    
-    #Install library
-    source activate $CONDA_RAY_ENV
-    python3 -m pip install git+https://github.com/asnaylor/nersc_cluster_deploy.git
-    python3 -m ipykernel install --user --name $CONDA_RAY_ENV --display-name $CONDA_RAY_ENV
-    source deactivate
-}
+setup_env() {
+  ML_MODULE=$1
+  module load $ML_MODULE
+  echo "<> Setting up $ML_MODULE env"
 
-setup_env(){
-    IMAGE_NAME=$1
-    JUPYTER_KERNEL=$(echo $IMAGE_NAME | sed -r 's#[/:]+#_#g')
-    JUPYTER_KERNEL_FOLDER=$HOME/.local/share/jupyter/kernels/$JUPYTER_KERNEL/
-    CUSTOM_PYTHONUSERBASE=$HOME/.local/perlmutter/$JUPYTER_KERNEL
-
-    if [ "$2" == "true" ]
-    then
-      echo "<!> Force clearing PYTHONUSERBASE + kernel.json"
-      rm -rf $CUSTOM_PYTHONUSERBASE
-      rm -rf $JUPYTER_KERNEL_FOLDER
-    fi
-}
-
-shifter_ml_image(){
-    shifter --image=$IMAGE_NAME --module=gpu,nccl-2.15 \
-            --env PYTHONUSERBASE=$CUSTOM_PYTHONUSERBASE \
-            "$@"
-}
-
-setup_shifter_kernel(){
-    CUSTOM_PYTHON_BIN=$1
-    
-    #check folder exists
-    if [ -d "$JUPYTER_KERNEL_FOLDER" ]; then
-      echo "<!> $JUPYTER_KERNEL is already setup..."
+  if python -m pip freeze | grep -q nersc-cluster-deploy
+    then  
+      echo "<!> nersc_cluster_deploy is already setup..."
       return
-    fi
+  fi
 
-    ## Copy files across
-    mkdir -p $JUPYTER_KERNEL_FOLDER
-    cp .kernel.json $JUPYTER_KERNEL_FOLDER/kernel.json
-    sed -i -e "s#CUSTOM_KERNEL_NAME#$JUPYTER_KERNEL#g" $JUPYTER_KERNEL_FOLDER/kernel.json
-    sed -i -e "s#CUSTOM_SHIFTER_IMAGE#$IMAGE_NAME#g" $JUPYTER_KERNEL_FOLDER/kernel.json
-    sed -i -e "s#CUSTOM_PYTHON_BIN#$CUSTOM_PYTHON_BIN#g" $JUPYTER_KERNEL_FOLDER/kernel.json
-    sed -i -e "s#CUSTOM_PYTHONUSERBASE#$CUSTOM_PYTHONUSERBASE#g" $JUPYTER_KERNEL_FOLDER/kernel.json
+  echo "<> Install nersc_cluster_deploy"
+  if [ "$DEV_FLAG" == "true" ]
+  then
+    pushd ../nersc_cluster_deploy
+    python -m pip install -e .
+    popd
+  else
+    python -m pip install git+https://github.com/asnaylor/nersc_cluster_deploy.git
+  fi
 
-    ## Install library
-    mkdir -p $CUSTOM_PYTHONUSERBASE
-    shifter --image=$IMAGE_NAME --module=gpu,nccl-2.15 \
-            --env PYTHONUSERBASE=$CUSTOM_PYTHONUSERBASE \
-            python3 -m pip install git+https://github.com/asnaylor/nersc_cluster_deploy.git --user
+  echo "<> Install additional requirements"
+  python -m pip install -r requirements.txt
 }
 
-setup_shifter_hvd_pytorch(){
-    #check if horovod installed
-    if shifter_ml_image python -m pip freeze | grep -q horovod
+setup_hvd(){
+  ML_FRAMEWORK=$1
+  HVD_OPT=${ML_FRAMEWORK^^}
+  if python -m pip freeze | grep -q horovod
     then  
       echo "<!> horovod is already setup..."
       return
-    fi
-
-    shifter --image=$IMAGE_NAME --module=gpu,nccl-2.15 \
-            --env PYTHONUSERBASE=$CUSTOM_PYTHONUSERBASE \
-            --env HOROVOD_NCCL_HOME=/opt/udiImage/modules/nccl-2.15 \
-            --env HOROVOD_GPU_OPERATIONS=NCCL \
-            --env HOROVOD_NCCL_LINK=SHARED \
-            --env HOROVOD_WITH_PYTORCH=1 \
-            python3 -m pip install horovod[pytorch]
-
+  fi
+  
+  HOROVOD_NCCL_HOME=$NCCL_HOME \
+  HOROVOD_GPU_OPERATIONS=NCCL \
+  HOROVOD_WITH_$HVD_OPT=1 \
+  python3 -m pip install horovod[$ML_FRAMEWORK]
 }
 
-
 EX_NUM=$1
-if [ "$2" == "-f" ]; then FORCE_FLAG=true; else FORCE_FLAG=false; fi
+if [ "$2" == "--dev" ]; then DEV_FLAG=true; else DEV_FLAG=false; fi
 
 case $EX_NUM in
 
   1)
-    echo "<> Setting up Ex1: PyTorch MNIST Example: Ray + Horovod"
-    setup_env nersc/pytorch:ngc-22.09-v0 $FORCE_FLAG
-    setup_shifter_kernel /opt/conda/bin/python
-    setup_shifter_hvd_pytorch
-    shifter_ml_image python -m pip install "ray[air]==2.3.0"
+    echo "<> Setting up Ex1: Tuning Hyperparameters of a Distributed PyTorch Model with PBT using Ray Train & Tune"
+    setup_env pytorch/1.13.1
     ;;
 
   2)
-    echo "<> Setting up Ex2: Tuning Hyperparameters of a Distributed PyTorch Model with PBT using Ray Train & Tune"
-    setup_env nersc/pytorch:ngc-22.09-v0 $FORCE_FLAG
-    setup_shifter_kernel /opt/conda/bin/python
-    shifter_ml_image python -m pip install "ray[air]==2.3.0"
+    echo "<> Setting up Ex2: Tuning Hyperparameters of a Distributed TensorFlow Model using Ray Train & Tune"
+    setup_env tensorflow/2.9.0
     ;;
 
   3)
-    echo "<> Setting up Ex3: Tuning Hyperparameters of a Distributed TensorFlow Model using Ray Train & Tune"
-    setup_env nersc/tensorflow:ngc-22.09-tf2-v0 $FORCE_FLAG
-    setup_shifter_kernel /usr/bin/python
-    shifter_ml_image python -m pip install "ray[air]==2.3.0"
+    echo "<> Setting up Ex3: PyTorch MNIST Example: Ray + Horovod"
+    setup_env pytorch/1.13.1
+    setup_hvd pytorch
     ;;
 
   *)
